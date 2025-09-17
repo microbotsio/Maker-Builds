614315b63aad7b65e46d7c083f53a590bbea2b83
microbots <info@microbots.io>
2025-09-17 17:44:29 +0200

    Update Motion_Alarm.ino

--- Motion Alarm/Software/Motion_Alarm/Motion_Alarm.ino
+++ Motion Alarm/Software/Motion_Alarm/Motion_Alarm.ino
@@ -1,5 +1,17 @@
-// Motion Alarm Example
-// Requires CodeCell Library v1.3.4 or later
+/*
+  Example: CodeCell Motion Alarm
+
+  This example turns your CodeCell into a motion alarm
+  - The onboard motion sensor detects movement and triggers an alarm sound.
+  - The alarm status (Enabled/Disabled) can be controlled via the MicroLink app.
+  - Button A (in the app) toggles the alarm ON or OFF.
+  - Button B (in the app) puts the CodeCell to sleep, reducing power consumption.
+
+  When motion is detected:
+  - The CodeCell lights up red.
+  - A DriveCell (connected to a CoilPad) plays a tone to alert for motion.
+  - A notification message is sent to the MicroLink app.
+*/
 
 #include <CodeCell.h>
 #include <DriveCell.h>
@@ -16,7 +28,6 @@ MicroLink myMicroLink;
 bool alarm_enable = 0;
 uint8_t motion_state = 0;
 char myMessage[18];
-float Roll, Pitch, Yaw;
 
 void setup() {
   Serial.begin(115200);  // Start the serial monitor at 115200 baud
@@ -28,15 +39,22 @@ void setup() {
     myCodeCell.Motion_Init(MOTION_STATE);
     myCodeCell.Motion_Read();  //Read value from motion sensor
     motion_state = myCodeCell.Motion_StateRead();
+    delay(1000);  //wait
     while (motion_state == MOTION_STATE_UNKNOWN) {
-      delay(10);                 //wait
+      Serial.println("Reading Sensor..");
+      delay(1000);               //wait
       myCodeCell.Motion_Read();  //Read value from motion sensor
       motion_state = myCodeCell.Motion_StateRead();
     }
-    if (motion_state == MOTION_STATE_ONTABLE) {
-      myCodeCell.Sleep(1);  //Go back to sleep & check again after 1 sec
+    if (motion_state != MOTION_STATE_MOTION) {
+      Serial.println("Alarm Not Triggered");
+      Serial.println("Going to Back to Sleep");
+      delay(1000);               //wait
+      myCodeCell.SleepTimer(1);  //Go back to sleep & check again after 1 sec
+    } else {
+      Serial.println("Alarm Triggered");
+      alarm_enable = 1;  //Turn on Alarm
     }
-    alarm_enable = 1;  //Turn on Alarm
   }
 
   if (alarm_enable) {
@@ -55,7 +73,7 @@ void setup() {
         delay(1000);
         myMicroLink.Print("Alarm Enabled");  //Print on MicroLink App Screen
         delay(1000);
-        myCodeCell.Sleep(1); /*Go to sleep & check proximity after 1 sec*/
+        myCodeCell.SleepTimer(1); /*Go to sleep & check proximity after 1 sec*/
       }
     }
   } else {
@@ -67,6 +85,7 @@ void setup() {
     myDriveCell.Drive(0, 0);              // Turn off Driver
     myMicroLink.Print("Alarm Disabled");  //Print on MicroLink App Screen
   }
+  delay(1000);
 }
 
 void loop() {
@@ -83,12 +102,25 @@ void loop() {
         myMicroLink.Print("Alarm Disabled");
         myDriveCell.Drive(0, 0);
       }
-      delay(1000);
+      delay(500);
     }
 
+    if (myMicroLink.ReadButtonB()) {
+      //Going to Sleep
+      myDriveCell.Drive(0, 0);              // Turn off Driver
+      myMicroLink.Print("Going To Sleep");  //Print on MicroLink App Screen
+      Serial.println("Going To Sleep");     //Print on MicroLink App Screen
+      delay(1000);
+      myMicroLink.Print("Alarm Enabled");  //Print on MicroLink App Screen
+      Serial.println("Alarm Enabled");     //Print on MicroLink App Screen
+      delay(1000);
+      myCodeCell.SleepTimer(1); /*Go to sleep & check proximity after 1 sec*/
+    }
+
+    motion_state = myCodeCell.Motion_StateRead();
     if (alarm_enable) {
-      motion_state = myCodeCell.Motion_StateRead();
-      if (motion_state != MOTION_STATE_ONTABLE) {
+      if (motion_state == MOTION_STATE_MOTION) {
+        Serial.println("Alarm Triggered");
         myMicroLink.Print("Alarm Triggered");
         myCodeCell.LED(100, 0, 0);
 
@@ -101,16 +133,9 @@ void loop() {
             delay(1000);
           }
         }
-      }
-    } else {
-      if (myMicroLink.ReadButtonB()) {
-        //Going to Sleep
-        myDriveCell.Drive(0, 0);              // Turn off Driver
-        myMicroLink.Print("Going To Sleep");  //Print on MicroLink App Screen
-        delay(1000);
-        myMicroLink.Print("Alarm Enabled");  //Print on MicroLink App Screen
-        delay(1000);
-        myCodeCell.Sleep(1); /*Go to sleep & check proximity after 1 sec*/
+
+      } else {
+        //wait
       }
     }
   }
